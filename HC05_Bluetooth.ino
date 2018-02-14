dc58bda8b6e8ec54b58ce80dd4f0daaa0667ccb6
Mohsin Alam <mohsin.alam@netsoltech.com>
2018-02-14 11:25:04 +0500

    Code file for Arduino

---
+++ HC05_Bluetooth.ino
@@ -0,0 +1,54 @@
+char junk;
+char inputString;
+
+void setup()                    // run once, when the sketch starts
+{
+  Serial.begin(9600  , SERIAL_8N1);           // set the baud rate to 9600, same should be of your Serial Monitor
+  pinMode(13, OUTPUT);
+  pinMode(12,OUTPUT);
+}
+
+void loop()
+{
+  if (Serial.available() > 0) 
+  {
+    while (Serial.available())
+    {
+      char inChar = (char)Serial.read(); //read the input
+      inputString += inChar;        //make a string of the characters coming on serial
+    }
+    
+    while (Serial.available() > 0)
+    {
+      junk = Serial.read() ;  // clear the serial buffer
+      Serial.println(junk);
+    }
+    
+    Serial.flush();
+
+    Serial.println(inputString);
+
+    if (inputString == 'a') // Lock Door
+    {
+      digitalWrite(13, HIGH);
+    }
+    else if (inputString == 'b') // unlock Door
+    {
+      digitalWrite(13, LOW);
+    }
+    else if (inputString == 'c') // Blink lights
+    {
+      digitalWrite(12, LOW);
+      delay(500);
+      digitalWrite(12, HIGH);
+      delay(500);
+      digitalWrite(12, LOW);
+      delay(500);
+      digitalWrite(12, HIGH);
+      delay(500);
+      digitalWrite(12, LOW);
+    }
+
+    inputString = 0;
+  }
+}
