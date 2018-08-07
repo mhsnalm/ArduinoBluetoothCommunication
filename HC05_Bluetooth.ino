15f6f8c8883ea21c53b78ca9ac5c284cc230dd29
Mohsin Alam <mohsin.alam@netsoltech.com>
2018-08-07 18:21:28 +0500

    Updated for reverse polarity actuators

--- HC05_Bluetooth.ino
+++ HC05_Bluetooth.ino
@@ -4,8 +4,9 @@ char inputString;
 void setup()                    // run once, when the sketch starts
 {
   Serial.begin(9600  , SERIAL_8N1);           // set the baud rate to 9600, same should be of your Serial Monitor
-  pinMode(13, OUTPUT);
+  pinMode(13,OUTPUT);
   pinMode(12,OUTPUT);
+  pinMode(10,OUTPUT);
 }
 
 void loop()
@@ -31,22 +32,27 @@ void loop()
     if (inputString == 'a') // Lock Door
     {
       digitalWrite(13, HIGH);
+      digitalWrite(12, LOW);
     }
     else if (inputString == 'b') // unlock Door
     {
       digitalWrite(13, LOW);
+      digitalWrite(12, HIGH);
     }
     else if (inputString == 'c') // Blink lights
     {
-      digitalWrite(12, LOW);
+      digitalWrite(10, LOW);
       delay(500);
-      digitalWrite(12, HIGH);
+      digitalWrite(10, HIGH);
       delay(500);
-      digitalWrite(12, LOW);
+      digitalWrite(10, LOW);
       delay(500);
-      digitalWrite(12, HIGH);
+      digitalWrite(10, HIGH);
       delay(500);
-      digitalWrite(12, LOW);
+      digitalWrite(10, LOW);
+           delay(500);
+      digitalWrite(10, HIGH);
+ 
     }
 
     inputString = 0;
